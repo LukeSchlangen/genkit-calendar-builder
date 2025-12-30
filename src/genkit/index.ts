cb86bbf8a1c4f431105f26b5bceeb074acca8ffe
Luke Schlangen <luke@example.com>
2025-12-30 21:03:14 +0000

    move all flows to single file

--- src/genkit/index.ts
+++ src/genkit/index.ts
@@ -1,7 +1,61 @@
-import { calendarIdeationFlow  } from './calendarIdeationFlow';
-import { calendarDateImagesFlow } from './calendarDatesImagesFlow';
+import { vertexAI } from '@genkit-ai/google-genai';
+import { genkit, z } from 'genkit';
 
-export {
-    calendarIdeationFlow,
-    calendarDateImagesFlow,
-}
+const ai = genkit({
+    plugins: [vertexAI()],
+});
+
+
+const ImportantDatesInputSchema = ai.defineSchema('ImportantDatesInputSchema', z.object({ theme: z.string() }));
+
+const ImportantDatesOutputSchema = ai.defineSchema('ImportantDatesOutputSchema', z.array(
+    z.object({
+        date: z.string(),
+        event: z.string(),
+    })
+));
+
+const ImportantDatesPrompt = ai.prompt<
+    typeof ImportantDatesInputSchema,
+    typeof ImportantDatesOutputSchema
+>('calendar-ideation');
+
+
+export const calendarIdeationFlow = ai.defineFlow(
+    {
+        name: 'calendarIdeationFlow',
+        inputSchema: ImportantDatesInputSchema,
+        outputSchema: ImportantDatesOutputSchema,
+    },
+    async ({ theme }) => {
+        const { text } = await ImportantDatesPrompt({ theme });
+        const result = ImportantDatesOutputSchema.parse(JSON.parse(text));
+        return result;
+    }
+);
+
+export const calendarDatesImagesFlow = ai.defineFlow(
+    {
+        name: 'calendarDatesImagesFlow',
+        inputSchema: ImportantDatesInputSchema,
+        outputSchema: ImportantDatesOutputSchema,
+    },
+    async ({ theme }) => {
+        const { text } = await ImportantDatesPrompt({ theme });
+
+        return JSON.parse(text);
+    }
+);
+
+export const completeCalendarGenerationFlow = ai.defineFlow(
+    {
+        name: 'completeCalendarGenerationFlow',
+        inputSchema: ImportantDatesInputSchema,
+        // outputSchema: ImportantDatesOutputSchema,
+    },
+    async ({ theme }) => {
+        const calendarIdeationFlowResults = await calendarIdeationFlow({ theme });
+        const calendarDatesImagesFlowResults = await calendarDatesImagesFlow({ theme });
+        return { calendarIdeationFlowResults, calendarDatesImagesFlowResults };
+    }
+);
